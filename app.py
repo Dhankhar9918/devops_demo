cf5bd335749623966d08dbb716c2b6deb9ef1051
dheeraj <dheerajdhankhar5@gmail.com>
2026-09-17 09:24:02 +0530

    modeified app file

---
+++ app.py
@@ -0,0 +1,13 @@
+
+def greet_user(name):
+    return f"Hello, {name}! Welcome to DevOps Demo."
+
+
+if __name__ == "__main__":
+    print(greet_user("Dheeraj"))
+    
+def add_numbers(a, b):
+    return a + b
+
+
+print("Sum:", add_numbers(10, 20))
